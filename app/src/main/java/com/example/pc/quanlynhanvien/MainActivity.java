4c7fb7cc28a70d2da61f8b2071d5a4636316d9ab
nguyenddpd01982 <nguyenddpd01982@fpt.edu.vn>
2018-04-26 01:05:51 +0700

    push by Nguyenddpd01982

--- app/src/main/java/com/example/pc/quanlynhanvien/MainActivity.java
+++ app/src/main/java/com/example/pc/quanlynhanvien/MainActivity.java
@@ -7,13 +7,35 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btAdmin, btManage, btEmployees;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        btAdmin = (Button) findViewById(R.id.btAdmin);
+        btManage = (Button) findViewById(R.id.btManage);
+        btEmployees = (Button) findViewById(R.id.btEmployees);
+        btAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,AdminActivity.class);
+                startActivity(intent);
+            }
+        });
+        btManage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,QLNVActivity.class);
+                startActivity(intent);
+            }
+        });
+        btEmployees.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,NVActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
